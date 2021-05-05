70b941a61003b230e2179ebb69b67f2906eeb463
Anjali Goyal <anjali.goyal@infobeans.com>
2021-05-05 19:57:58 +0530

    Partial login and navbar

--- infobeans-home/src/Login.js
+++ infobeans-home/src/Login.js
@@ -5,7 +5,35 @@ import "./Login.css";
 const Login = () => {
   return (
     <div className="login">
-      <div className="login__form"></div>
+      <div className="login__form">
+        <img
+          className="login__formLogo"
+          src="https://infobeans-design-system.web.app/images/logo-infobeans-black.svg"
+          alt="logo"
+        />
+        <label for="login__email">Email</label>
+        <input
+          className="text-input-placeholder"
+          type="text"
+          id="login__email"
+          name="login__email"
+          placeholder="Your InfoBeans email address"
+        />
+        <label className="stretch" for="login__password">
+          Password <a href="#">Forgot?</a>
+        </label>
+        <input
+          className="text-input-placeholder"
+          type="text"
+          id="login__password"
+          name="login__password"
+          placeholder="Your password"
+        />
+        <button className="inverse">Login to Intranet Portal</button>
+      </div>
+      <footer className="login__footer">
+        &copy; Copyright 2020 InfoBeans. All Rights Reserved.
+      </footer>
     </div>
   );
 };
--- infobeans-home/src/Nav.js
+++ infobeans-home/src/Nav.js
@@ -3,7 +3,21 @@ import React from "react";
 import "./Nav.css";
 
 const Nav = () => {
-  return <div className="nav"></div>;
+  return (
+    <header className="nav">
+      <div className="nav__logoWrapper">
+        <img
+          className="nav__logo"
+          src="https://infobeans-design-system.web.app/images/logo-infobeans-white.svg"
+          alt="logo"
+        />
+        <div className="text-portal nav__portal">Intranet Portal</div>
+      </div>
+      <div className="nav__menu">
+        <i className="ibic-apps nav__menuLink"></i>
+      </div>
+    </header>
+  );
 };
 
 export default Nav;
