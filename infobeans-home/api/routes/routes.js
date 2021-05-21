0a989e465798cedf7a32ebe9e39803bfe501705c
Anjali Goyal <anjali.goyal@infobeans.com>
2021-05-21 20:49:01 +0530

    Minor modifications

--- infobeans-home/api/routes/routes.js
+++ infobeans-home/api/routes/routes.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
 const mailer = require("../controller/mailControl");
 const upload = require("../config/multer");
+const auth = require("../config/auth");
 
 const Users = require("../model/userSchema");
 const Jobs = require("../model/jobSchema");
 const FormContacts = require("../model/contactSchema");
 
-const auth = require("../config/auth");
 const router = express.Router();
+dotenv.config();
 
 router.get("/jobs", auth, (req, res) => {
   Jobs.find({}, (err, data) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      jwt.verify(req.token, "secretkey", (err, authData) => {
+      jwt.verify(req.token, process.env.SECRET_KEY, (err, authData) => {
         if (err) {
           res.sendStatus(403);
         } else {
@@ -37,7 +39,7 @@ router.post("/login", (req, res) => {
       } else {
         if (data.length) {
           const user = data[0];
-          jwt.sign({ user }, "secretkey", (err, token) => {
+          jwt.sign({ user }, process.env.SECRET_KEY, (err, token) => {
             res.send({ token: token });
           });
         } else {
@@ -50,7 +52,13 @@ router.post("/login", (req, res) => {
 
 router.post("/form", upload.single("file"), (req, res) => {
   const formData = req.body;
-  formData.file = req.file.path;
+  const port = process.env.PORT || 9000;
+  const url = `http://localhost:${port}`;
+
+  formData.file = `${url}/${req.file.path}`.replace("\\", "/");
+
+  // console.log(formData.file);
+  // http://localhost:9000/uploads/1621608183857-Screenshot.PNG
 
   FormContacts.create(formData, (err, data) => {
     if (err) {
